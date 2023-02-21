190032f5870683dff058a7f39a1ec4f6dd634763
Dilawar Singh <dilawar@subcom.tech>
2023-02-21 20:45:45 +0530

    chore: fixes to disqus

--- disqus.js
+++ disqus.js
@@ -1,7 +1,3 @@
-document.addEventListener("load", function() {
-    const disqus = document.createElement("div").id("disqus_thread");
-    document.body.appendChild(disqus);
-});
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
 s.src = 'https://dilawars-me.disqus.com/embed.js';
