7d7da6f9bc53bfea67195063f741ff0a32e988e8
Dilawar Singh <dilawar@subcom.tech>
2023-03-11 11:58:23 +0530

    chore: typo fix.

---
+++ disqus.js
@@ -0,0 +1,10 @@
+document.addEventListener("load", function() {
+    const disqus = document.createElement("div").id("disqus_thread");
+    document.body.appendChild(disqus);
+});
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://dilawars-me.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
